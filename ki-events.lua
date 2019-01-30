2a10da8179d3f1131b0906ccbdc259b9c618e675
Andrew Kwon <andrewshky@gmail.com>
2019-01-30 00:01:34 -0800

    Rework entity creation & add some new ones

--- ki-events.lua
+++ ki-events.lua
@@ -1,5 +1,6 @@
-local function createApplicationShortcuts(Application)
-    local iinaActions = {
+-- Create IINA application entity
+local function createIINA(Application)
+    local actions = {
         playNext = Application.createMenuItemEvent("Next Media"),
         playPrevious = Application.createMenuItemEvent("Previous Media"),
         togglePlay = Application.createMenuItemEvent({ "Pause", "Resume" }, {
@@ -7,9 +8,29 @@ local function createApplicationShortcuts(Application)
             focusBefore = true,
         }),
     }
+    local shortcuts = {
+        { nil, "space", actions.togglePlay, { "Playback", "Toggle Play" } },
+        { nil, "p", actions.playPrevious, { "Playback", "Previous Media" } },
+        { nil, "n", actions.playNext, { "Playback", "Next Media" } },
+    }
+
+    return Application:new("IINA", shortcuts)
+end
+
+-- Create iTerm application entity
+local function createiTerm(Application)
     local iTermActions = {
         toggleFullScreen = Application.createMenuItemEvent("Toggle Full Screen", { focusBefore = true })
     }
+    local shortcuts = {
+        { nil, "f", iTermActions.toggleFullScreen, { "View", "Toggle Full Screen" } },
+    }
+
+    return Application:new("iTerm", shortcuts)
+end
+
+-- Create Microsoft Outlook application entity
+local function createMicrosoftOutlook(Application)
     local outlookActions = {
         switchToViewHandler = function(view)
             return function()
@@ -23,44 +44,59 @@ local function createApplicationShortcuts(Application)
             end
         end
     }
-
-    return {
-        IINA = {
-            { nil, "space", iinaActions.togglePlay, { "Playback", "Toggle Play" } },
-            { nil, "p", iinaActions.playPrevious, { "Playback", "Previous Media" } },
-            { nil, "n", iinaActions.playNext, { "Playback", "Next Media" } },
-        },
-        iTerm = {
-            { nil, "f", iTermActions.toggleFullScreen, { "View", "Toggle Full Screen" } },
-        },
-        MicrosoftOutlook = {
-            { nil, "c", outlookActions.switchToViewHandler("calendar"), { "View", "Switch to Calendar View" } },
-            { nil, "m", outlookActions.switchToViewHandler("mail"), { "View", "Switch to Mail View" } },
-        },
+    local shortcuts = {
+        { nil, "c", outlookActions.switchToViewHandler("calendar"), { "View", "Switch to Calendar View" } },
+        { nil, "m", outlookActions.switchToViewHandler("mail"), { "View", "Switch to Mail View" } },
     }
+
+    return Application:new("Microsoft Outlook", shortcuts)
 end
 
+-- Create VLC application entity
+local function createVLC(Application)
+    local function executeApplescriptEvent(script)
+        return function()
+            return hs.osascript.applescript(script)
+        end
+    end
+    local actions = {
+        openFile = Application.createMenuItemEvent("Open File..."),
+        playPause = executeApplescriptEvent([[ tell application "VLC" to play ]]),
+        stop = executeApplescriptEvent([[ tell application "VLC" to stop ]]),
+    }
+    local shortcuts = {
+        { nil, "space", actions.playPause, { "Playback", "Toggle Play" } },
+        { nil, "o", actions.openFile, { "File", "Open File" } },
+        { nil, "s", actions.stop, { "Playback", "Stop" } },
+    }
+
+    return Application:new("VLC", vlcShortcuts)
+end
+
+-- Create custom Ki workflow events
 local function createWorkflowEvents(Ki)
     local File = Ki.File
     local Application = Ki.Application
-    local shortcuts = createApplicationShortcuts(Application)
 
     local Alacritty = Application:new("Alacritty")
     local Discord = Application:new("Discord")
-    local IINA = Application:new("IINA", shortcuts.IINA)
-    local iTerm = Application:new("iTerm", shortcuts.iTerm)
+    local IINA = createIINA(Application)
+    local iTerm = createiTerm(Application)
     local Hammerspoon = Application:new("Hammerspoon")
     local Slack = Application:new("Sblack")
     local ScriptEditor = Application:new("Script Editor")
-    local MicrosoftOutlook = Application:new("Microsoft Outlook", shortcuts.MicrosoftOutlook)
+    local MicrosoftOutlook = createMicrosoftOutlook(Application)
     local MicrosoftExcel = Application:new("Microsoft Excel")
+    local MicrosoftWord = Application:new("Microsoft Word")
     local Postico = Application:new("Postico")
     local TablePlus = Application:new("TablePlus")
     local VMWareFusion = Application:new("VMware Fusion")
+    local VLC = createVLC(Application)
 
     local entityWorkflowEvents = {
         { nil, "a", Alacritty, { "Entities", "Alacritty" } },
         { nil, "e", MicrosoftExcel, { "Entities", "Microsoft Excel" } },
+        { nil, "w", MicrosoftWord, { "Entities", "Microsoft Word" } },
         { nil, "v", VMWareFusion, { "Entities", "VMware Fusion" } },
         { { "ctrl" }, "s", ScriptEditor, { "Entities", "Script Editor" } },
         { { "shift" }, "d", Discord, { "Entities", "Discord" } },
@@ -68,6 +104,7 @@ local function createWorkflowEvents(Ki)
         { { "shift" }, "h", Hammerspoon, { "Entities", "Hammerspoon" } },
         { { "shift" }, "p", TablePlus, { "Entities", "TablePlus" } },
         { { "shift" }, "t", iTerm, { "Entities", "iTerm" } },
+        { { "shift" }, "v", VLC, { "Entities", "VLC" } },
         { { "shift", "cmd" }, "m", MicrosoftOutlook, { "Entities", "Microsoft Outlook" } },
         { { "shift", "cmd" }, "p", Postico, { "Entities", "Postico" } },
         { { "shift", "cmd" }, "s", Slack, { "Entities", "Sblack" } },
@@ -75,8 +112,10 @@ local function createWorkflowEvents(Ki)
 
     local selectEntityWorkflowEvents = {
         { nil, "e", MicrosoftExcel, { "Select Events", "Microsoft Excel" } },
+        { nil, "w", MicrosoftWord, { "Select Events", "Microsoft Word" } },
         { nil, "v", VMWareFusion, { "Select Events", "VMware Fusion" } },
         { { "ctrl" }, "s", ScriptEditor, { "Select Events", "Script Editor" } },
+        { { "shift" }, "v", VLC, { "Select Events", "VLC" } },
         { { "shift" }, "i", IINA, { "Select Events", "Select IINA Windows" } },
         { { "shift" }, "t", iTerm, { "Select Events", "iTerm" } },
         { { "shift", "cmd" }, "m", MicrosoftOutlook, { "Select Events", "Microsoft Outlook" } },
@@ -97,6 +136,8 @@ local function createWorkflowEvents(Ki)
 
     local fileWorkflowEvents = {
         { nil, "c", File:new("~/Code"), { "Files", "Code" } },
+        { nil, "v", File:new("~/Documents/Virtual Machines"), { "Files", "Virtual Machines" } },
+        { { "alt" }, "d", File:new("~/Dropbox"), { "Files", "Dropbox" } },
     }
 
     return {
