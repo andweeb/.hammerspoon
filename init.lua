969216a136fc83c901600165e1829271bf4dc6e3
Andrew Kwon <andrewshky@gmail.com>
2019-01-30 00:30:04 -0800

    Allow merging of untracked local workflow events

--- init.lua
+++ init.lua
@@ -1,14 +1,27 @@
-require "window-resizer"
-local createWorkflowEvents = require "ki-events"
+require("window-resizer")
+
+local createWorkflowEvents = require("ki-events")
 
 -- Reload Hammerspoon hotkey
-hs.hotkey.bind({"alt", "cmd", "shift"}, "r", function() hs.reload() end)
+hs.hotkey.bind({ "alt", "cmd", "shift" }, "r", function() hs.reload() end)
 
 -- Load & initialize Ki spoon
 hs.loadSpoon("Ki")
 
+local workflowEvents = createWorkflowEvents(spoon.Ki)
+
+-- Merge any untracked local workflow events to the list of default ones if a local file exists
+if io.open("local-ki-events.lua", "rb") then
+    local createLocalWorkflowEvents = require("local-ki-events")
+    local localWorkflowEvents = createLocalWorkflowEvents(spoon.Ki)
+
+    setmetatable(localWorkflowEvents, spoon.Ki:_createEventsMetatable(true))
+
+    workflowEvents = localWorkflowEvents + workflowEvents
+end
+
 -- Set custom workflows
-spoon.Ki.workflowEvents = createWorkflowEvents(spoon.Ki)
+spoon.Ki.workflowEvents = workflowEvents
 
 -- Start Ki
 spoon.Ki:start()
