e19a71ba8af9d8f2c803cc181127117e4ad87f29
Andrew Kwon <andrewshky@gmail.com>
2019-03-10 18:23:17 -0700

    Add custom TablePlus shortcuts

--- ki-events.lua
+++ ki-events.lua
@@ -73,6 +73,69 @@ local function createVLC(Application)
     return Application:new("VLC", vlcShortcuts)
 end
 
+-- Create TablePlus application entity
+local function createTablePlus(Application)
+    local actions = {}
+
+    local dataPath = "~/Library/Application\\ Support/com.tinyapp.TablePlus/Data"
+    local function readTablePlusPlist(filename)
+        local json = hs.execute("plutil -convert json -o - "..dataPath.."/"..filename)
+        return hs.json.decode(json)
+    end
+
+    -- Initialize connection groups by group id and connections list
+    local connectionGroups = {}
+    local connectionGroupList = readTablePlusPlist("ConnectionGroups.plist")
+    for _, group in pairs(connectionGroupList) do
+        connectionGroups[group.ID] = group.Name
+    end
+    connections = readTablePlusPlist("Connections.plist")
+
+    function actions.open(app, choice)
+        app:activate()
+
+        if choice then
+            local isOk, _, rawTable = hs.osascript.applescript([[
+                open location "tableplus://?id=]]..choice.id..[["
+            ]])
+
+            if not isOk then
+                Application.notifyError("Error opening the connection", rawTable.NSLocalizedFailureReason)
+            end
+        end
+    end
+
+    local shortcuts = {
+        { nil, nil, actions.open, { "TablePlus", "Open Application or Database Connection" } },
+    }
+
+    local TablePlus = Application:new("TablePlus", shortcuts)
+
+    function TablePlus.getSelectionItems()
+        local choices = {}
+
+        for _, connection in pairs(connections) do
+            local group = connectionGroups[connection.GroupID]
+            local text = connection.ConnectionName.." ["..connection.Enviroment.."]"
+            local databaseInfoText = connection.DatabaseHost.." : "..connection.DatabaseName
+            local subText = hs.styledtext.new(databaseInfoText, {
+                color = { hex = connection.statusColor, alpha = 1 },
+                font = { name = "Menlo", size = 10 },
+            })
+
+            table.insert(choices, {
+                text = text,
+                subText = subText,
+                id = connection.ID,
+            })
+        end
+
+        return choices
+    end
+
+    return TablePlus
+end
+
 -- Create ClipboardText entity
 local function createClipboardTextEntity(Entity)
     local function updateTextCaseEvent(case)
@@ -112,17 +175,19 @@ local function createWorkflowEvents(Ki)
     -- Applications
     local Alacritty = Application:new("Alacritty")
     local Discord = Application:new("Discord")
-    local IINA = createIINA(Application)
-    local iTerm = createiTerm(Application)
     local Hammerspoon = Application:new("Hammerspoon")
     local Sblack = Application:new("Sblack")
     local ScriptEditor = Application:new("Script Editor")
-    local MicrosoftOutlook = createMicrosoftOutlook(Application)
     local MicrosoftExcel = Application:new("Microsoft Excel")
     local MicrosoftWord = Application:new("Microsoft Word")
     local Postico = Application:new("Postico")
-    local TablePlus = Application:new("TablePlus")
     local VMWareFusion = Application:new("VMware Fusion")
+
+    -- Applications with custom shortcuts
+    local IINA = createIINA(Application)
+    local iTerm = createiTerm(Application)
+    local MicrosoftOutlook = createMicrosoftOutlook(Application)
+    local TablePlus = createTablePlus(Application)
     local VLC = createVLC(Application)
 
     -- Custom Desktop Entities
@@ -151,9 +216,10 @@ local function createWorkflowEvents(Ki)
         { nil, "w", MicrosoftWord, { "Select Events", "Microsoft Word" } },
         { nil, "v", VMWareFusion, { "Select Events", "VMware Fusion" } },
         { { "ctrl" }, "s", ScriptEditor, { "Select Events", "Script Editor" } },
-        { { "shift" }, "v", VLC, { "Select Events", "VLC" } },
         { { "shift" }, "i", IINA, { "Select Events", "Select IINA Windows" } },
+        { { "shift" }, "p", TablePlus, { "Select Events", "Select a DB Connection with TablePlus" } },
         { { "shift" }, "t", iTerm, { "Select Events", "iTerm" } },
+        { { "shift" }, "v", VLC, { "Select Events", "VLC" } },
         { { "shift", "cmd" }, "m", MicrosoftOutlook, { "Select Events", "Microsoft Outlook" } },
     }
 
