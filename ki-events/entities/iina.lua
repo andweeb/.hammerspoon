1281eb6db3f9d22847b58f5ced21eca33e305fe5
Andrew Kwon <andrewshky@gmail.com>
2019-06-05 22:58:10 -0700

    Add "Open Recent" actions to VLC and IINA entities

--- ki-events/entities/iina.lua
+++ ki-events/entities/iina.lua
@@ -9,10 +9,34 @@ function IINAEntity:init(Application)
             focusBefore = true,
         }),
     }
+
+    function actions.openRecent(app)
+        local menuItem = { "File", "Open Recent" }
+        local menuItemList = Application.getMenuItemList(app, menuItem)
+        local recentFileChoices = {}
+
+        for _, item in pairs(menuItemList) do
+            if item.AXTitle and #item.AXTitle > 0 then
+                table.insert(recentFileChoices, {
+                    text = item.AXTitle,
+                })
+            end
+        end
+
+        local function selectMenuItemAndFocus(choice)
+            table.insert(menuItem, choice.text)
+            app:selectMenuItem(menuItem)
+            Application.focus(app)
+        end
+
+        Application.showSelectionModal(recentFileChoices, selectMenuItemAndFocus)
+    end
+
     local shortcuts = {
         { nil, "space", actions.togglePlay, { "Playback", "Toggle Play" } },
         { nil, "p", actions.playPrevious, { "Playback", "Previous Media" } },
         { nil, "n", actions.playNext, { "Playback", "Next Media" } },
+        { { "shift" }, "o", actions.openRecent, { "Playback", "Open Recent" } },
     }
 
     return Application:new("IINA", shortcuts)
--- ki-events/entities/vlc.lua
+++ ki-events/entities/vlc.lua
@@ -6,15 +6,40 @@ function VLCEntity:init(Application)
             return hs.osascript.applescript(script)
         end
     end
+
     local actions = {
         openFile = Application.createMenuItemEvent("Open File..."),
         playPause = executeApplescriptEvent([[ tell application "VLC" to play ]]),
         stop = executeApplescriptEvent([[ tell application "VLC" to stop ]]),
     }
+
+    function actions.openRecent(app)
+        local menuItem = { "File", "Open Recent" }
+        local menuItemList = Application.getMenuItemList(app, menuItem)
+        local recentFileChoices = {}
+
+        for _, item in pairs(menuItemList) do
+            if item.AXTitle and #item.AXTitle > 0 then
+                table.insert(recentFileChoices, {
+                    text = item.AXTitle,
+                })
+            end
+        end
+
+        local function selectMenuItemAndFocus(choice)
+            table.insert(menuItem, choice.text)
+            app:selectMenuItem(menuItem)
+            Application.focus(app)
+        end
+
+        Application.showSelectionModal(recentFileChoices, selectMenuItemAndFocus)
+    end
+
     local shortcuts = {
         { nil, "space", actions.playPause, { "Playback", "Toggle Play" } },
         { nil, "o", actions.openFile, { "File", "Open File" } },
         { nil, "s", actions.stop, { "Playback", "Stop" } },
+        { { "shift" }, "o", actions.openRecent, { "Playback", "Open Recent" } },
     }
 
     return Application:new("VLC", shortcuts)
