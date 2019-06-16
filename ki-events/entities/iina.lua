c5fd3dcfa66833f3cd10303ed982f1e7feba2553
Andrew Kwon <andrewshky@gmail.com>
2019-06-16 15:33:26 -0700

    Add more IINA menu item events

--- ki-events/entities/iina.lua
+++ ki-events/entities/iina.lua
@@ -1,9 +1,16 @@
 local IINAEntity = {}
 
 function IINAEntity:init(Application)
+    self.Application = Application
+
     local actions = {
+        open = Application.createMenuItemEvent("Open...", { focusAfter = true }),
+        openURL = Application.createMenuItemEvent("Open URL...", { focusAfter = true }),
         playNext = Application.createMenuItemEvent("Next Media"),
         playPrevious = Application.createMenuItemEvent("Previous Media"),
+        playbackHistory = Application.createMenuItemEvent("Playback History", {
+            focusAfter = true,
+        }),
         togglePlay = Application.createMenuItemEvent({ "Pause", "Resume" }, {
             isToggleable = true,
             focusBefore = true,
@@ -24,9 +31,11 @@ function IINAEntity:init(Application)
         end
 
         local function selectMenuItemAndFocus(choice)
-            table.insert(menuItem, choice.text)
-            app:selectMenuItem(menuItem)
-            Application.focus(app)
+            if choice then
+                table.insert(menuItem, choice.text)
+                app:selectMenuItem(menuItem)
+                Application.focus(app)
+            end
         end
 
         Application.showSelectionModal(recentFileChoices, selectMenuItemAndFocus)
@@ -34,9 +43,13 @@ function IINAEntity:init(Application)
 
     local shortcuts = {
         { nil, "space", actions.togglePlay, { "Playback", "Toggle Play" } },
+        { nil, "h", actions.playbackHistory, { "File", "Playback History" } },
         { nil, "p", actions.playPrevious, { "Playback", "Previous Media" } },
         { nil, "n", actions.playNext, { "Playback", "Next Media" } },
-        { { "shift" }, "o", actions.openRecent, { "Playback", "Open Recent" } },
+        { nil, "o", actions.open, { "File", "Open..." } },
+        { nil, "y", actions.playbackHistory, { "File", "Playback History" } },
+        { { "shift" }, "o", actions.openRecent, { "File", "Open Recent" } },
+        { { "cmd", "shift" }, "o", actions.openURL, { "File", "Open URL..." } },
     }
 
     return Application:new("IINA", shortcuts)
