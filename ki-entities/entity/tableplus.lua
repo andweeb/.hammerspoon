bfb6cf72c28894c66ecded85fdeb55bc2e9af738
Andrew Kwon <andrewshky@gmail.com>
2019-11-01 16:05:07 -0700

    Improve TablePlus entity implementation

--- ki-entities/entity/tableplus.lua
+++ ki-entities/entity/tableplus.lua
@@ -3,47 +3,29 @@
 --
 local Application = spoon.Ki.Application
 
-local actions = {}
+local TablePlus = Application:new("TablePlus")
+TablePlus.dataPath = "~/Library/Application\\ Support/com.tinyapp.TablePlus/Data"
 
-local dataPath = "~/Library/Application\\ Support/com.tinyapp.TablePlus/Data"
-local function readTablePlusPlist(filename)
-    local json = hs.execute("plutil -convert json -o - "..dataPath.."/"..filename)
-    return hs.json.decode(json)
+-- Validate the data path
+local dataPath = TablePlus.dataPath:gsub("%\\", "")
+local absolutePath = hs.fs.pathToAbsolute(dataPath)
+local success, value = pcall(function() return hs.fs.attributes(absolutePath) or {} end)
+if not success then
+    TablePlus.notifyError("TablePlus data path not found.", value or "")
 end
 
--- Initialize connection groups by group id and connections list
-local connectionGroups = {}
-local connectionGroupList = readTablePlusPlist("ConnectionGroups.plist")
-for _, group in pairs(connectionGroupList) do
-    connectionGroups[group.ID] = group.Name
+function TablePlus:readPlist(name)
+    local json = hs.execute("plutil -convert json -o - "..self.dataPath.."/"..name..".plist")
+    return json and hs.json.decode(json) or {}
 end
-local connections = readTablePlusPlist("Connections.plist")
-
-function actions.open(app, choice)
-    app:activate()
-
-    if choice then
-        local isOk, _, rawTable = hs.osascript.applescript([[
-            open location "tableplus://?id=]]..choice.id..[["
-        ]])
-
-        if not isOk then
-            Application.notifyError("Error opening the connection", rawTable.NSLocalizedFailureReason)
-        end
-    end
-end
-
-local shortcuts = {
-    { nil, nil, actions.open, { "TablePlus", "Open Application or Database Connection" } },
-}
-
-local TablePlus = Application:new("TablePlus", shortcuts)
 
 function TablePlus.getSelectionItems()
     local choices = {}
 
+    -- Initialize connection groups by group id and connections list
+    local connections = TablePlus:readPlist("Connections")
+
     for _, connection in pairs(connections) do
-        local group = connectionGroups[connection.GroupID]
         local text = connection.ConnectionName.." ["..connection.Enviroment.."]"
         local databaseInfoText = connection.DatabaseHost.." : "..connection.DatabaseName
         local subText = hs.styledtext.new(databaseInfoText, {
@@ -60,5 +42,27 @@ function TablePlus.getSelectionItems()
 
     return choices
 end
+
+function TablePlus.open(app, choice)
+    app:activate()
+
+    if choice then
+        local isOk, _, rawTable = hs.execute([[
+            open "tableplus://?id=]]..choice.id..[["
+        ]])
+
+        if not isOk then
+            Application.notifyError("Error opening the connection", rawTable.NSLocalizedFailureReason)
+        end
+    end
+end
+
+local shortcuts = {
+    { nil, nil, TablePlus.open, { "TablePlus", "Open Application or Database Connection" } },
+}
+
+-- Initialize TablePlus
+TablePlus.connections = TablePlus:readPlist("Connections")
+TablePlus.shortcuts = TablePlus.mergeShortcuts(shortcuts, TablePlus.shortcuts)
 
 return TablePlus
