315381d96b1a28b4a47880e4342d0dcb3cf8be13
Andrew Kwon <andrewshky@gmail.com>
2019-11-06 23:01:30 -0800

    Refactor Java entity & allow registering app names

--- ki-entities/entity/java.lua
+++ ki-entities/entity/java.lua
@@ -2,42 +2,67 @@
 -- Java .jar file application
 --
 local Application = spoon.Ki.Application
+local Java = Application:new("Java")
 
-local actions = {}
+Java.javaAppNames = { "java" }
 
-function actions.getQuitMenuItem(app)
+Java.behaviors = Java.behaviors + {
+    entity = function(_, eventHandler)
+        eventHandler()
+        return true
+    end
+}
+
+function Java:getApps()
+    local apps = {}
+
+    for _, registeredAppName in pairs(self.javaAppNames) do
+        local app = hs.application.find(registeredAppName)
+        if app then table.insert(apps, app) end
+    end
+
+    return apps
+end
+
+function Java:registerJavaApps(appNames)
+    for _, appName in pairs(appNames) do
+        table.insert(self.javaAppNames, appName)
+    end
+end
+
+function Java.getQuitMenuItem(app)
     local menuItems = app:getMenuItems()
     local appMenuItems = menuItems[1].AXChildren[1]
     return appMenuItems[#appMenuItems]
 end
 
-function actions.getName(app)
+function Java:getName(app)
     local defaultName = "java"
 
     -- Get java app name from last "Quit" menu item
-    local quitMenuItem = actions.getQuitMenuItem(app)
+    local quitMenuItem = self.getQuitMenuItem(app)
     local appName = quitMenuItem.AXTitle:match(" (.*)")
 
     return appName or defaultName
 end
 
-function actions.getSelectionItems(_, apps)
-    apps = apps or { hs.application.find("java") }
+function Java:getSelectionItems(apps)
+    apps = apps or self:getApps()
 
     local choices = {}
 
     for index, javaApp in pairs(apps) do
         table.insert(choices, {
             index = index,
-            text = actions.getName(javaApp),
+            text = self:getName(javaApp),
         })
     end
 
     return choices
 end
 
-function actions.focus(_, choice)
-    local apps = { hs.application.find("java") }
+function Java.focus(_, choice)
+    local apps = Java:getApps()
 
     if choice then
         return Application.focus(apps[choice.index])
@@ -47,13 +72,10 @@ function actions.focus(_, choice)
 end
 
 local shortcuts = {
-    { nil, nil, actions.focus, { "Java", "Activate/Focus Java App" } },
+    { nil, nil, Java.focus, { "Java", "Activate/Focus Java App" } },
+    { nil, "a", function() print("bitch") end }
 }
 
-local Java = Application:new("java", shortcuts)
-
-for name, action in pairs(actions) do
-    Java[name] = action
-end
+Java:initialize("Java", shortcuts)
 
 return Java
