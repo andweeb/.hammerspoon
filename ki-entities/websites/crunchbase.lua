887ce18e56f411769f1aeeb9b50a10c6fe0f4f26
Andrew Kwon <andrewshky@gmail.com>
2022-01-30 13:33:06 -0800

    Add website entities and fix Dropbox shorcut

---
+++ ki-entities/websites/crunchbase.lua
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------------------------------------
+-- Crunchbase website config
+--
+local Ki = spoon.Ki
+local Website = Ki.Website
+local URLSearchMixin = require("ki-entities/url-search")
+local url = "https://www.crunchbase.com"
+
+local Crunchbase = Website {
+    name = "Crunchbase",
+    url = url,
+}
+
+function Crunchbase.getCompanyInfo()
+    return hs.osascript.applescript([[
+        set info to ""
+
+        tell application "Safari"
+            tell current tab of window 1
+                set info to do JavaScript "test=`${document.querySelector('.field-type-date_precision').innerText} / ${Array.from(document.querySelectorAll('.component--field-formatter.field-type-enum.link-accent.ng-star-inserted')).map(e => e.innerText).join(' / ')} / ${document.querySelector('.component--field-formatter.field-type-money.ng-star-inserted').innerText}`" as text
+            end tell
+        end tell
+
+        return info
+    ]])
+end
+
+function Crunchbase:copyCompanyInfo()
+    local isOk, _, output = self.getCompanyInfo()
+    if isOk then
+        hs.pasteboard.setContents(output)
+        hs.notify.show("Ki", "Copied copy company info to clipboard", "")
+    else
+        hs.notify.show("Ki", "Unable to copy company info to clipboard", "")
+    end
+end
+
+Crunchbase:registerShortcuts({
+    { nil, "c", function() Crunchbase:copyCompanyInfo() end },
+})
+
+-- Initialize website instance with search mixins
+Crunchbase.class:include(URLSearchMixin)
+Crunchbase:initURLSearch("Search/results.htm", "keyword")
+
+function Crunchbase:basicURLSearch(encodedQuery)
+    local formatURL = "https://www.google.com/search?q=%s%%20site%%3Acrunchbase.com"
+    local searchURL = string.format(formatURL, encodedQuery)
+    return self.open(searchURL)
+end
+
+return Crunchbase
---
+++ ki-entities/websites/glassdoor.lua
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------------------------
+-- Glassdoor website config
+--
+local Ki = spoon.Ki
+local Website = Ki.Website
+local URLSearchMixin = require("ki-entities/url-search")
+local url = "https://www.glassdoor.com"
+
+local Glassdoor = Website {
+    name = "Glassdoor",
+    url = url,
+    links = {
+        { name = "Profile", link = "/member/profile/index.htm" },
+        { name = "Resumes", link = "/member/resumes/index.htm" },
+        { name = "Account Settings", link = "/member/account/settings_input.htm" },
+    },
+}
+
+function Glassdoor.getCompanyInfo()
+    return hs.osascript.applescript([[
+        set info to ""
+
+        tell application "Safari"
+            tell current tab of window 1
+                set info to do JavaScript "test=`${document.querySelector('.css-1c86vvj').innerText} (${document.querySelector('.eiCell.cell.reviews > .num.eiHeaderLink').innerText})`" as text
+            end tell
+        end tell
+
+        return info
+    ]])
+end
+
+function Glassdoor:copyCompanyInfo()
+    local isOk, _, output = self.getCompanyInfo()
+    if isOk then
+        hs.pasteboard.setContents(output)
+        hs.notify.show("Ki", "Copied copy company info to clipboard", "")
+    else
+        hs.notify.show("Ki", "Unable to copy company info to clipboard", "")
+    end
+end
+
+Glassdoor:registerShortcuts({
+    { nil, "c", function() Glassdoor:copyCompanyInfo() end },
+})
+
+-- Initialize website instance with search mixins
+Glassdoor.class:include(URLSearchMixin)
+Glassdoor:initURLSearch("Search/results.htm", "keyword")
+
+return Glassdoor
