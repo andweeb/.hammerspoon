048ff83a643efdec987866a4684d7435fb44bab7
Andrew Kwon <andrewshky@gmail.com>
2022-08-14 21:42:52 -0700

    Add Google Flights and My Maps

--- ki-entities/websites/google.lua
+++ ki-entities/websites/google.lua
@@ -6,4 +6,14 @@ local Google = spoon.Ki.defaultEntities.website.Google
 
 Google.class:include(URLSearchMixin)
 
+local links = {
+    { name = "Google Flights", link = "https://www.google.com/travel/flights" },
+    { name = "Google My Maps", link = "https://mymaps.google.com" },
+}
+
+for i = 1, #links do
+    local link = links[i]
+    table.insert(Google.links, link)
+end
+
 return Google
